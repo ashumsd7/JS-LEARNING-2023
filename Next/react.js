0ea58bf91bb8fc1b1a875de36deccd8b885fe4b1
aathym <aat8001@gmail.com>
2022-12-26 16:50:47 +0530

    react js to next js

--- Next/react.js
+++
@@ -1,9 +0,0 @@
-const app = document.getElementById("app");
-ReactDOM.render(<h1>Hey I am rendered by the react js.</h1>, app);
-
-//always capital
-function Header() {
-  return <h2>I am component.🚀 </h2>;
-}
-
-ReactDOM.render(<Header/>, app);
