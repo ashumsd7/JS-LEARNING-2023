21c72ad696890713d58cb87a1cce5bb23da632df
aat <aat8001@gmail.com>
2023-03-23 10:23:14 +0530

    hoisting

---
+++ Course-JS-Deep-1/hoisiting.js
@@ -0,0 +1,17 @@
+
+
+console.log(color)
+
+var color='blue' 
+
+
+// function Expression ( hoisted like var let and const)
+var getProduct = function(num1, num2){
+    //do something
+}
+
+//function Declaration ( Hoisted with the definition)
+
+function getProduct(num1, num2){
+
+}
