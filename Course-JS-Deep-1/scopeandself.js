6045170ad4ca2c6f874abd13149f95531daa5442
aat <aat8001@gmail.com>
2023-03-24 00:31:49 +0530

    fix: self and scope

---
+++ Course-JS-Deep-1/scopeandself.js
@@ -0,0 +1,15 @@
+var myCar = {
+    color: "Blue",
+    logColor: function() {
+        var self = this;
+        console.log("In logColor - this.color: " + this.color);
+        console.log("In logColor - self.color: " + self.color);
+        (function() {
+            console.log("In IIFE - this.color: " + this.color);
+            console.log("In IIFE - self.color: " + self.color);
+            console.log(this)
+        })();
+    }
+};
+
+myCar.logColor();
